2edf7cb78469f73d6f8b328d62ca40d5b3c93f03
Justin Wozniak <woz@anl.gov>
2024-07-26 12:02:01 -0500

    Adding spearman.py

---
+++ ranking/spearman/spearman.py
@@ -0,0 +1,72 @@
+
+import csv
+
+import pandas as pd
+
+"""
+Headers:
+0: model
+1: cell line
+2: drug
+3: mean(predicted_auc)
+4: std(predicted_auc)
+5: mean(actual_auc)
+6: std(actual_auc)
+7: num_observations
+"""
+
+def parse_args():
+    import argparse
+    parser = argparse.ArgumentParser(
+        prog="spearman",
+        description="Run modified spearman ranking analysis.")
+    parser.add_argument("tsv",
+                        help="The input TSV")
+    parser.add_argument("cell",
+                        help="The cell line to analyze")
+    parser.add_argument("count",
+                        type=int,
+                        help="Number of top drugs to consider (n)")
+    args = parser.parse_args()
+    return args
+
+
+args = parse_args()
+n = args.count
+
+df = pd.DataFrame(csv.reader(open(args.tsv), delimiter="\t"))
+
+print("original dataframe length: %i" % len(df))
+
+# The selected rows:
+rows = df.loc[df[1] == args.cell]
+# print("matching rows for cell '%s': %i" % (args.cell, len(rows)))
+
+# Sort to copy and update index
+rank_predict = rows.sort_values(3, ignore_index=True)
+# print(str(rank_predict[0:n]))
+
+# Sort to copy and update index
+rank_actual = rows.sort_values(5, ignore_index=True)
+# print(str(rank_actual[0:n]))
+
+# Basic sum of differences (Σ d)
+sum1 = 0
+# Sum of difference squares (Σ d^2)
+sum2 = 0
+
+for i in range(0, n):
+    drug = rank_predict.iloc[i][2]
+    #print("drug ", drug)
+    match = rank_predict.loc[rank_actual[2] == drug]
+    # print("match ", match.index.item())
+    diff  = abs(i - match.index.item())
+    sum1 += diff
+    diff2 = diff ** 2
+    # print("diff2 ", diff2)
+    sum2 += diff2
+    # print("match ", str(match), " is ", )
+
+spearman = 1 - 6*diff2/(n*(n**2 - 1))
+print("mae:      %6.3f" % (sum1/n))
+print("spearman: %6.3f" % spearman)
