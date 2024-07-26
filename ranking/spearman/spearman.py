3d8fbe8479b4ba3d43f4ec94048b5fe89fe1428c
Justin Wozniak <woz@anl.gov>
2024-07-26 15:32:01 -0500

    Run one-sided actual against predicted; start on clustered

--- ranking/spearman/spearman.py
+++ ranking/spearman/spearman.py
@@ -54,17 +54,63 @@ def spearman(df, cell, n):
     sum2 = 0
 
     for i in range(0, n):
-        drug = rank_predict.iloc[i][2]
+        drug = rank_actual.iloc[i][2]
         #print("drug ", drug)
-        match = rank_predict.loc[rank_actual[2] == drug]
+        match = rank_actual.loc[rank_predict[2] == drug]
         # print("match ", match.index.item())
         diff  = abs(i - match.index.item())
         sum1 += diff
         diff2 = diff ** 2
-        # print("diff2 ", diff2)
+        print("diff2 ", diff2)
         sum2 += diff2
         # print("match ", str(match), " is ", )
 
     mae = (sum1/n)
-    spearman = 1 - 6*diff2/(n*(n**2 - 1))
+    denominator = (n*(n**2 - 1))
+    spearman = 1 - 6*diff2 / denominator
+    print("denominator: %i" % denominator)
+    return (mae, spearman)
+
+def spearman_clustered(df, cell, n):
+    """
+    df: The dataframe (see headers above)
+    cell: The cell line string
+    n: The count of drugs of interest
+    """
+
+    print("original dataframe length: %i" % len(df))
+
+    # The selected rows:
+    rows = df.loc[df[1] == cell]
+    # print("matching rows for cell '%s': %i" % (args.cell, len(rows)))
+
+    # Sort to copy and update index
+    rank_predict = rows.sort_values(3, ignore_index=True)
+    # print(str(rank_predict[0:n]))
+
+    # Sort to copy and update index
+    rank_actual = rows.sort_values(5, ignore_index=True)
+    # print(str(rank_actual[0:n]))
+
+    # Basic sum of differences (Σ d)
+    sum1 = 0
+    # Sum of difference squares (Σ d^2)
+    sum2 = 0
+
+    for i in range(0, n):
+        drug = rank_actual.iloc[i][2]
+        #print("drug ", drug)
+        match = rank_actual.loc[rank_predict[2] == drug]
+        # print("match ", match.index.item())
+        diff  = abs(i - match.index.item())
+        sum1 += diff
+        diff2 = diff ** 2
+        print("diff2 ", diff2)
+        sum2 += diff2
+        # print("match ", str(match), " is ", )
+
+    mae = (sum1/n)
+    denominator = (n*(n**2 - 1))
+    spearman = 1 - 6*diff2 / denominator
+    print("denominator: %i" % denominator)
     return (mae, spearman)
